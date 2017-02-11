60382e81c3249f8245aa143a93ecf60fa56d63c4
Lorenzo Martinico <lzmartinico@gmail.com>
2017-02-11 12:28:18 -0500

    Add decreasing halo sizes

---
+++ Client/Assets/Scripts/ChangeHaloSize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeHaloSize : MonoBehaviour {
+
+	public Transform target;
+	public float speed;
+	public float originalRange;
+	public Light lt;
+	public float past = 0;
+  void Start() {
+         lt = GetComponent<Light>();
+         originalRange = lt.range;
+				 Debug.Log(originalRange);
+  }
+	void Update() {
+		past += Time.deltaTime;
+		float step = speed * past;
+	  lt.range = originalRange-step;
+	}
+}
