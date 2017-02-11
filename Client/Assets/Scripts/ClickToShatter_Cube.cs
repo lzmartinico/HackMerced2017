cfced2e62dd2b4d8ca659dc3b9c036213a0d3e7f
Lorenzo Martinico <lzmartinico@gmail.com>
2017-02-11 13:53:16 -0500

    Changed cubes into spheres

--- Client/Assets/Scripts/ClickToShatter_Cube.cs
+++ Client/Assets/Scripts/ClickToShatter_Cube.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ClickToShatter_Cube : MonoBehaviour
 {
 	public GameObject Cube_Fragments;
-	void OnMouseOver () {
+		void OnMouseOver () {
 		if (Input.GetMouseButtonDown(1)) {
 			Instantiate(Cube_Fragments, transform.position, transform.rotation);
 			Destroy(gameObject);
