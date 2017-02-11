544a630a5c806e5b9d4f44a7aca3f66b97ea723c
Lorenzo Martinico <lzmartinico@gmail.com>
2017-02-11 15:27:15 -0500

    Changed song, halo intensity

--- Client/Assets/Scripts/ChangeHaloSize.cs
+++ Client/Assets/Scripts/ChangeHaloSize.cs
@@ -7,18 +7,20 @@ public class ChangeHaloSize : MonoBehaviour {
 	public Transform target;
 	public float speed;
 	public float originalRange;
+	public float originalIntensity;
 	public Light lt;
 	public float past = 0;
   void Start() {
          lt = GetComponent<Light>();
          originalRange = lt.range;
-				 Debug.Log(originalRange);
+		originalIntensity = lt.intensity;
   }
 	void Update() {
 		past += Time.deltaTime;
 		float step = speed * past;
 	  lt.range = originalRange-step;
-		if (lt.range <= 0) {
+		lt.intensity = originalIntensity - step;
+		if (lt.range <= 1) {
 			Destroy (gameObject);
 		}
 	}
