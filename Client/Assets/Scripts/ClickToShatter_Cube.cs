6492132a31ae059ad41f6624c49d078d5acf175b
Roman Pshichenko <Pshichenko@hotmail.com>
2017-02-11 02:37:37 -0800

    Click to Shatter | Scene | Prefabs

---
+++ Client/Assets/Scripts/ClickToShatter_Cube.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToShatter_Cube : MonoBehaviour
+{
+	public GameObject Cube_Fragments;
+	void OnMouseOver () {
+		if (Input.GetMouseButtonDown(1)) {
+			Instantiate(Cube_Fragments, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Client/Assets/Scripts/DestroyOnTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTimer : MonoBehaviour {
+
+	public float min_time = 0.5f;
+	public float max_time = 2.0f;
+	private float _life_remaining;
+
+	// Use this for initialization
+	void Start () {
+		_life_remaining = Random.Range (min_time, max_time);
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		if (_life_remaining <= 0) {
+			Destroy(gameObject);
+		} else {
+			_life_remaining -= Time.deltaTime;
+		}
+	}
+}
