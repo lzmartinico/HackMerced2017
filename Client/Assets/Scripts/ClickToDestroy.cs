55073d0ef01490e6e77fe3289cd9f3030f9698fb
Roman Pshichenko <Pshichenko@hotmail.com>
2017-02-11 01:32:14 -0800

    Click to Destory | Scene | Script

---
+++ Client/Assets/Scripts/ClickToDestroy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToDestroy : MonoBehaviour
+{
+	void OnMouseOver () {
+		if (Input.GetMouseButtonDown(1)) {
+			Destroy(gameObject);
+		}
+	}
+}
