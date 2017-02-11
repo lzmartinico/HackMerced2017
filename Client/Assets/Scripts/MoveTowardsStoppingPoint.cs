8c82e438de3ac770c7aef541647dd4fb3a3cc831
Roman Pshichenko <Pshichenko@hotmail.com>
2017-02-11 11:28:07 -0800

    Move Towards Stopping Point | Added spawner to scene

--- Client/Assets/Scripts/MoveTowardsStoppingPoint.cs
+++ Client/Assets/Scripts/MoveTowardsStoppingPoint.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveTowardsTarget : MonoBehaviour {
-	
-	public Transform target;
+public class MoveTowardsStoppingPoint : MonoBehaviour {
+
 	public float speed;
+	private Transform target;
+
+	void Start () {
+		GameObject[] stoppingPoints = GameObject.FindGameObjectsWithTag ("StoppingPoint");
+		target = stoppingPoints [0].transform;
+	}
 
 	void Update() {
 		float step = speed * Time.deltaTime;
